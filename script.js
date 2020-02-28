c5d188e588e12161d9acf54d7636302f2b998536
Gretszam Zabek <gz01056@srcs.loc>
2020-02-28 13:23:59 -0800

    sth

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function motivQuote(feeling) {
     var happy = ["You do you!", "Just keep keeping on", "you are doing great, you got this"];
     var sad = ["Tomorrow is a new day", "Clouds always pass", "Emotions are not destinations"];
     var angry = ["Just breath", "Visualize your paradise", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR8xB4IJztY1tYyEu4iT4_pxHQBy34g3PragTbD9ASC-vYj5jt0"];
-<<<<<<< HEAD
 
     if(feeling = Sad){
         var sQuote = document.createElement("p");
@@ -15,8 +14,4 @@ function motivQuote(feeling) {
         sQuote.innerHTML = sad[x];
         body.appendChild(sQuote);
        }
-
-=======
->>>>>>> master/master
 }
-
