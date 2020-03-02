23c63e55eac1f316bc9855828775eecfdce60861
Phoebe Johnson <phoebejohnson@phoebes-air.srcs.loc>
2020-03-02 10:26:18 -0800

    rdytvbj

--- script.js
+++ script.js
@@ -5,16 +5,13 @@ button.addEventListener("click", () => {
 
 function motivQuote(feeling) {
 
-    console.log("feeling", feeling);
-
     var happy = ["You do you!", "Just keep keeping on", "you are doing great, You got this"];
-    var happy = ["You do you!", "Just keep keeping on", "you are doing great, you got this"];
     var sad = ["Tomorrow is a new day", "Clouds always pass", "Emotions are not destinations"];
     var angry = ["Just breath", "Visualize your paradise", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR8xB4IJztY1tYyEu4iT4_pxHQBy34g3PragTbD9ASC-vYj5jt0"];
 
-    var s = document.getElementById("emotion").options.namedItem("2");
-    var h = document.getElementById("emotion").options.namedItem("1");
-    var a = document.getElementById("emotion").options.namedItem("3");
+    var s = document.getElementById("emotion").value;
+    var h = document.getElementById("emotion").value;
+    var a = document.getElementById("emotion").value;
 
     if (feeling == s) {
         var sQuote = document.getElementById("words");
