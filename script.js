e0b5f38f1a8ff16821eaf4e5d1caed79e8a45450
Phoebe Johnson <phoebejohnson@phoebes-air.srcs.loc>
2020-03-02 14:42:53 -0800

    rtdtfyguh

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function motivQuote(feeling) {
 
     var happy = ["You do you!", "Just keep keeping on", "you are doing great, You got this"];
     var sad = ["Tomorrow is a new day", "Clouds always pass", "Emotions are not destinations"];
-    var angry = ["Just breath", "Visualize your paradise", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR8xB4IJztY1tYyEu4iT4_pxHQBy34g3PragTbD9ASC-vYj5jt0"];
+    var angry = ["Just breath", "Visualize your paradise", "Clear your mind, revel in the emptiness"];
 
 
     if (feeling == "sad") {
