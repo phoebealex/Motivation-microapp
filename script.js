58b75b048f60a2130cb712b654973829daf1ff58
Gretszam <penguino11111@gmail.com>
2020-03-01 20:16:09 -0800

    minor fixes that messed with js, still not fixed

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
-let button = document.getElemenntById("roll")
-roll.addEventListener("click", () => {
+let button = document.getElementById('roll');
+
+button.addEventListener('click', () => {
     motivQuote(document.getElementById("dropdown").value)
-})
+});
 
 function motivQuote(feeling) {
 
 
 
-    var happy = ["You do you!", "Just keep keeping on", "you are doing great, you got this"];
+    var happy = ["You do you!", "Just keep keeping on", "you are doing great, You got this"];
     var sad = ["Tomorrow is a new day", "Clouds always pass", "Emotions are not destinations"];
     var angry = ["Just breath", "Visualize your paradise", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR8xB4IJztY1tYyEu4iT4_pxHQBy34g3PragTbD9ASC-vYj5jt0"];
 
-    var s = document.getElementById("emotion").options.namedItem("2");
-    var h = document.getElementById("emotion").options.namedItem("1");
-    var a = document.getElementById("emotion").options.namedItem("3");
+    var h = document.getElementById("emotion").option.namedItem("1");
+    var s = document.getElementById("emotion").option.namedItem("2");
+    var a = document.getElementById("emotion").option.namedItem("3");
 
-    if(feeling == s){
+    if (feeling == h) {
+        var hQuote = document.getElementById("words");
+        var x = Math.floor(Math.random() * 3);
+        hQuote.innerHTML = happy[x];
+        p.appendChild(hQuote);
+    } else if (feeling == s) {
         var sQuote = document.getElementById("words");
         var x = Math.floor(Math.random() * 3);
         sQuote.innerHTML = sad[x];
         p.appendChild(sQuote);
-       }
-    else if(feeling == h){
-        var hQuote = document.getElementById("words");
-        var x = Math.floor(Math.random() * 3);
-        sQuote.innerHTML = happy[x];
-        body.appendChild(hQuote);
-    }
-    else if(feeling == a){
+    } else if (feeling == a) {
         var aQuote = document.getElementById("words");
         var x = Math.floor(Math.random() * 3);
-        sQuote.innerHTML = angry[x];
+        aQuote.innerHTML = angry[x];
         p.appendChild(aQuote);
     }
 }
