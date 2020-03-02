7eca79e70391845dc846ee979574fa647583c39c
Phoebe Johnson <phoebejohnson@phoebes-air.srcs.loc>
2020-03-02 10:18:02 -0800

    tykdx

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function motivQuote(feeling) {
 
     console.log("feeling", feeling);
 
+    var happy = ["You do you!", "Just keep keeping on", "you are doing great, You got this"];
     var happy = ["You do you!", "Just keep keeping on", "you are doing great, you got this"];
     var sad = ["Tomorrow is a new day", "Clouds always pass", "Emotions are not destinations"];
     var angry = ["Just breath", "Visualize your paradise", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR8xB4IJztY1tYyEu4iT4_pxHQBy34g3PragTbD9ASC-vYj5jt0"];
@@ -15,19 +16,27 @@ function motivQuote(feeling) {
     var h = document.getElementById("emotion").options.namedItem("1");
     var a = document.getElementById("emotion").options.namedItem("3");
 
-    if(feeling == s){
+    if (feeling == s) {
         var sQuote = document.getElementById("words");
         var x = Math.floor(Math.random() * 3);
         sQuote.innerHTML = sad[x];
         p.appendChild(sQuote);
+<<<<<<< HEAD
        }
     if(feeling == h){
+=======
+    } else if (feeling == h) {
+>>>>>>> master/master
         var hQuote = document.getElementById("words");
         var x = Math.floor(Math.random() * 3);
         hQuote.innerHTML = happy[x];
         p.appendChild(hQuote);
+<<<<<<< HEAD
     }
    if(feeling == a){
+=======
+    } else if (feeling == a) {
+>>>>>>> master/master
         var aQuote = document.getElementById("words");
         var x = Math.floor(Math.random() * 3);
         aQuote.innerHTML = angry[x];
