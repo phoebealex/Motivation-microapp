11836e0865dd7f0e34241c9332bfc92366baa4bb
Gretszam Zabek <gz01056@srcs.loc>
2020-03-02 10:36:26 -0800

    e

--- script.js
+++ script.js
@@ -9,25 +9,24 @@ function motivQuote(feeling) {
     var sad = ["Tomorrow is a new day", "Clouds always pass", "Emotions are not destinations"];
     var angry = ["Just breath", "Visualize your paradise", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR8xB4IJztY1tYyEu4iT4_pxHQBy34g3PragTbD9ASC-vYj5jt0"];
 
-    var s = document.getElementById("emotion").value;
     var h = document.getElementById("emotion").value;
+    var s = document.getElementById("emotion").value;
     var a = document.getElementById("emotion").value;
 
+
+    if (feeling == h) {
+        var hQuote = document.getElementById("words");
+        var x = Math.floor(Math.random() * 3);
+        hQuote.innerHTML = happy[x];
+        p.appendChild(hQuote);
+    }
     if (feeling == s) {
         var sQuote = document.getElementById("words");
         var x = Math.floor(Math.random() * 3);
         sQuote.innerHTML = sad[x];
         p.appendChild(sQuote);
     }
-    if (feeling == h) {} else if (feeling == h) {
-        var hQuote = document.getElementById("words");
-        var x = Math.floor(Math.random() * 3);
-        hQuote.innerHTML = happy[x];
-        p.appendChild(hQuote);
-    }
     if (feeling == a) {
-
-    } else if (feeling == a) {
         var aQuote = document.getElementById("words");
         var x = Math.floor(Math.random() * 3);
         aQuote.innerHTML = angry[x];
